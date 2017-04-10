aed7c68af68c3694096fe91b109429e02a5169fd
Iain Brown <Iain.Brown@gbgplc.com>
2017-04-10 17:06:33 +0100

    Update

--- client/src/providers/socket-service/socket-service.ts
+++ client/src/providers/socket-service/socket-service.ts
@@ -1,5 +1,4 @@
 import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import * as io from 'socket.io-client';
---
+++ typings/index.d.ts
@@ -0,0 +1,1 @@
+/// <reference path="globals/babylonjs/index.d.ts" />

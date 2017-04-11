448b74d4e65624d8f5d13f1f09105f584e4e8d40
Iain Brown <Iain.Brown@gbgplc.com>
2017-04-11 15:09:14 +0100

    Base core engine readt for wire up and testing

--- client/src/providers/GameService/Target.ts
+++ client/src/providers/GameService/Target.ts
@@ -3,35 +3,34 @@ import { Injectable } from '@angular/core'
 import { Game } from './Game'
 
 
-@Injectable()
-export class Target extends BABYLON.Mesh {
+var Target = function(game, posX, posZ) {
 
-    private game: Game;
+    this.game = game;
 
-    // constructor(game: Game, posX: number, posZ: number) {
-    //     this.game = game;
-    //     BABYLON.Mesh.call(this, "target", game.scene);
-    //     var vd = BABYLON.VertexData.CreateSphere(16);
-    //     vd.applyToMesh(this, false);
+    BABYLON.Mesh.call(this, "target", game.scene);
+    var vd = BABYLON.VertexData.CreateSphere(16);
+    vd.applyToMesh(this, false);
 
-    //     this.position
-    // }
-    init(game: Game, posX: number, posZ: number) {
-        this.game = game;
-        BABYLON.Mesh.call(this, "target", game.scene);
-        var vd = BABYLON.VertexData.CreateSphere(16);
-        vd.applyToMesh(this, false);
+    // The game
+    this.game = game;
 
-        this.position = new BABYLON.Vector3(posX, 4, posZ);
-        this.checkCollisions = true;
-        var _self = this;
-        this.game.scene.registerBeforeRender(function () {
-            _self.rotation.y += 0.01;
-        })
-    }
-    explode(){
-        this.dispose();
-    }
+    // Target position
+    this.position = new BABYLON.Vector3(posX, 4, posZ);
 
+    // Check collisions
+    this.checkCollisions = true;
 
-}
+    var _this = this;
+    this.game.scene.registerBeforeRender(function() {
+        _this.rotation.y += 0.01;
+    });
+};
+
+// Our object is a BABYLON.Mesh
+Target.prototype = Object.create(BABYLON.Mesh.prototype);
+// And its constructor is the Ship function described above.
+Target.prototype.constructor = Target;
+
+Target.prototype.explode = function() {
+    this.dispose();
+};

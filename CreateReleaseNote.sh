3122176cad64cb8478c7dccdfc688fede1c77096
Iain Brown <codeiain@outlook.com>
2017-04-24 08:58:09 +0100

    Cleaned up travis

--- CreateReleaseNote.sh
+++ CreateReleaseNote.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+echo "Installing npm packages"
+
+eval "npm install github-release-notes -g"
+
 echo "Createing Release Notes"
 
 eval "gren --username=codeiain --repo=MMORPG  --data-source=commits --override --tags=all"

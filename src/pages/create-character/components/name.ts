7c9c9c9f700ba5c696651de1541093c8414d178a
Iain Brown <codeiain@outlook.com>
2017-05-08 13:03:53 +0100

    #MMOC-12 fix

--- src/pages/create-character/components/name.ts
+++ src/pages/create-character/components/name.ts
@@ -1,15 +1,37 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { CharacterApiProvider } from '../../../providers/CharacterProviders/CharacterApiProvider'
+
+
 @Component({
   selector: 'page-create-character-name',
   templateUrl: 'name.html',
 })
 export class CreatCharacterName {
   @Output() updateCharacterName = new EventEmitter();
+  isValid: string = "form-control-danger";
+  isValidGroup: string = "has-danger";//
+  constructor(public apiProvider: CharacterApiProvider) {
+
+  }
 
   valuechange(newValue) {
+    var _self = this;
     this.updateCharacterName.emit(newValue);
+    this.apiProvider.verifyCharacterName(newValue).then((value: any) => {
+      if (value.name.length == 0) {
+        _self.isValid = "form-control-success"
+        _self.isValidGroup = "has-success"
+      } else {
+        _self.isValid = "form-control-danger"
+        _self.isValidGroup = "has-danger"
+      }
+    }, (err) => {
+      _self.isValid = "form-control-danger";
+      _self.isValidGroup = "has-danger"
+    })
+
   }
 
 }
